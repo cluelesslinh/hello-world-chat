0806bd69053033df643d0e297ded3abc6f37bbc4
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2021-11-15 10:11:05 -0800

    rebuild app

---
+++ README.md
@@ -0,0 +1,3 @@
+#Overview
+
+Hello-World is a chatting app developed using React Native. The app will provide users with a chat interface and options to share images and their location.
---
+++ src/components/Chat.js
@@ -0,0 +1,151 @@
+import React from "react";
+import { View, Platform, Button, KeyboardAvoidingView } from "react-native";
+
+const firebase = require("firebase");
+require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSMop6QwvxrwFJNiKfw7sqxXkuyUCXzoI",
+  authDomain: "hello-world-3a1d2.firebaseapp.com",
+  projectId: "hello-world-3a1d2",
+  storageBucket: "hello-world-3a1d2.appspot.com",
+  messagingSenderId: "330297885570"
+};
+
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+
+export default class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      uid: 0,
+      loggedInText: "Logging in...",
+      user: {
+        _id: "",
+        name: "",
+      },
+    };
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+
+    this.referenceChatMessages = firebase.firestore().collection("messages");
+    this.referenceMessageUser = null;
+  }
+
+  componentDidMount() {
+    const name = this.props.route.params.username;
+    this.props.navigation.setOptions({ title: name });
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user.uid,
+        messages: [],
+        user: {
+          _id: user.uid,
+          name: name,
+        },
+      });
+      this.referenceMessagesUser = firebase
+        .firestore()
+        .collection("messages")
+        .where("uid", "==", this.state.uid);
+
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    });
+  }
+
+  addMessage() {
+    const message = this.state.messages[0];
+    // add the new messages to the collection
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessage();
+      }
+    );
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+        },
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000",
+            // Background color for my own chat window. Could be adjusted to fit the color concept to a greenish tone
+          },
+        }}
+      />
+    );
+  }
+
+  componentWillUnmount() {
+    // stop authentication
+    this.authUnsubscribe();
+  }
+
+  render() {
+
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: this.props.route.params.backgroundColor,
+        }}>
+        <GiftedChat
+          messages={this.state.messages}
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={this.state.user}
+        />
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behaviour="height" />
+        ) : null}
+        {/* This part is important for Issues with Android Keyboard covering Chat window */}
+      </View>
+      // </View>
+    );
+  }
+}
